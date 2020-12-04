194ee5071db3a4b97ffb1746faf995916e53409e
WIN-OUTDV3ITTIV\Administrator <nerdlabenterprise@gmail.com>
2020-12-05 04:32:54 +0800

    Configuration file refactors.

--- config/ip-blocker.php
+++ config/ip-blocker.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * IP BLOCKER
- * ----------------------------------
- * List down all ip addresses to be
- * blocked by your application. Ip
- * blocking is implemented by the
- * IPBlockerMiddleware and will be
- * ignored if no ip address listed
- * below.
- */
+return [
 
-return [];
+
+];
